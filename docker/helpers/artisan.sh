1c8bdbb51cf3bd7456e100bf5ba34c1ac478c690
Attila Darok <ati@sysadmin.lan>
2020-04-26 13:57:22 +0200

    dynamic artisan helper, based on service id

--- docker/helpers/artisan.sh
+++ docker/helpers/artisan.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+root=$( cd "$(dirname "${BASH_SOURCE[0]}")/../.." ; pwd -P )
+
+service_id=$(cd $root && cd app && docker-compose ps -q app)
+
+if [ -z "$service_id" ]
+then
+	echo "Service (app) not running."
+	exit 1
+fi
+
 str="$*"
 
-docker exec -it remote_app php artisan $str
+docker exec -it $service_id php artisan $str
